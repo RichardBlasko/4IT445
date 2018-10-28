2b2eeda9f4b53a9deb0206fc49b313cea114b4b4
Nicole Gross <nicole.gross@student.tuke.sk>
2018-10-28 10:30:10 +0100

    feat: about page feat: admin home page

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -4,11 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import { PageWrapper } from './components/organisms/PageWrapper';
 
 import { HomePage } from './components/pages/HomePage';
+import { AboutPage } from './components/pages/AboutPage';
+import { AdminHomePage } from './components/pages/AdminHomePage';
 
 export const AppRoutes = () => (
   <PageWrapper>
     <Switch>
       <Route path="/" exact component={HomePage} />
+        <Route path="/about" exact component={AboutPage} />
+      <Route path="/admin" exact component={AdminHomePage} />
     </Switch>
   </PageWrapper>
 );
