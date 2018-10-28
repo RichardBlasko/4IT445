b8c6e69eba3deacbce13784cba8451ba1cfe6d6a
Ondrej Grof <grof@avast.com>
2018-10-28 15:10:47 +0100

    fix: conflicts

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -4,14 +4,22 @@ import { Switch, Route } from 'react-router-dom';
 import { PageWrapper } from './components/organisms/PageWrapper';
 
 import { HomePage } from './components/pages/HomePage';
-import { AboutPage } from './components/pages/AboutPage';
-import { AdminHomePage } from './components/pages/AdminHomePage';
+import {PreventionListPage} from "./components/pages/PreventionListPage";
+import {DiagnoseListPage} from "./components/pages/DiagnoseListPage";
+import {StoryListPage} from "./components/pages/StoryListPage";
+import {EventListPage} from "./components/pages/EventListPage";
+import {SocialNetworkListPage} from "./components/pages/SocialNetworkListPage";
+import {AdminHomePage} from "./components/pages/AdminHomePage";
 
 export const AppRoutes = () => (
   <PageWrapper>
     <Switch>
       <Route path="/" exact component={HomePage} />
-        <Route path="/about" exact component={AboutPage} />
+      <Route path="/pribehy" exact component={StoryListPage} />
+      <Route path="/prevence" exact component={PreventionListPage} />
+      <Route path="/kalendar" exact component={EventListPage} />
+      <Route path="/socialnisite" exact component={SocialNetworkListPage} />
+      <Route path="/diagnozy" exact component={DiagnoseListPage} />
       <Route path="/admin" exact component={AdminHomePage} />
     </Switch>
   </PageWrapper>
