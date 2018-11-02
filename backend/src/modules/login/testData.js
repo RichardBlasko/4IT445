be8a0d01f8461b11017155f454a37ccd36ede347
Richard Blasko <richard.blasko13@gmail.com>
2018-11-02 16:48:55 +0100

    Add basic backend login

---
+++ backend/src/modules/login/testData.js
@@ -0,0 +1,18 @@
+export const LOGINS = [
+
+{
+   id: 1,
+   login: 'doc1@doc.cz',
+   password: 123456,
+ },
+ {
+    id: 2,
+    login: 'doc2@doc.cz',
+    password: 654321,
+  },
+  {
+     id: 3,
+     login: 'doc3@doc.cz',
+     password: 123654,
+   },
+];
