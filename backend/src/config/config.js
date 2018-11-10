9795e4679cec85d24b51f93b1f39b245d03f04e1
Anastasi <a.korzhenevskaja@gmail.com>
2018-11-10 16:02:42 +0100

    ConfigJS + gitignore

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "development": {
+    "username": "team03",
+    "password": "xc39grikecld8fdg",
+    "database": "user_team03",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "team03",
+    "password": "xc39grikecld8fdg",
+    "database": "user_team03",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+};
