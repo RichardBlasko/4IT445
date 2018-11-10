80a214532198a8907fc2f088587cd2b1b6b8c1c6
Anastasi <a.korzhenevskaja@gmail.com>
2018-11-10 23:51:15 +0100

    api: insert prevence

--- backend/src/modules/prevence/prevenceController.js
+++ backend/src/modules/prevence/prevenceController.js
@@ -4,3 +4,14 @@ export const prevenceController = async (req, res) => {
   const prevence = await db.Prevence.findAll({});
   res.json({ prevence });
 };
+
+export const createPrevenceController =
+  async (req, res) => {
+
+  const newPrevence = await db.Prevence.create(req.body);
+
+  res.json({
+    ok: true,
+    newPrevence,
+  });
+};
--- backend/src/modules/prevence/routes.js
+++ backend/src/modules/prevence/routes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import {
   prevenceController,
+  createPrevenceController,
 } from './prevenceController';
 
 const router = Router();
 router.get('/', prevenceController);
 //router.get('/:id', productDetailController);
+router.post('/', createPrevenceController);
 
 export default router;
