f1d9830d84408bb5ef5a48138d479a2d12a85c1b
Nicole Gross <nicole.gross@student.tuke.sk>
2018-11-14 18:32:44 +0100

    frontend db fetch

--- frontend/src/store/createRootReducer.js
+++ frontend/src/store/createRootReducer.js
@@ -2,17 +2,17 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { shoppingCartReducer } from '../services/StoryList/reducer';
+import { preventionListReducer } from '../services/PreventionList/reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['shoppingCart'],
+  whitelist: ['preventionCard'],
 };
 
 export const createRootReducer = () => {
   const rootReducer = combineReducers({
-    shoppingCart: shoppingCartReducer,
+    preventionCard: preventionListReducer
   });
 
   return persistReducer(persistConfig, rootReducer);
