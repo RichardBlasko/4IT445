e0ce1f89f5a53f9e3c8734c0a91cd20b0a8f2320
Anastasi <a.korzhenevskaja@gmail.com>
2018-11-15 22:35:34 +0100

    feat: bulk update diagnozy

--- backend/src/modules/diagnozy/diagnozyController.js
+++ backend/src/modules/diagnozy/diagnozyController.js
@@ -1,5 +1,6 @@
 import db from '../../models/';
 
+
 export const diagnozyController = async (req, res) => {
   const diagnozy = await db.Diagnozy.findAll({});
   res.json({ diagnozy });
@@ -45,3 +46,44 @@ export const deleteDiagnozyController = async (req, res) => {
             res.status(500).json(error);
         });
 };
+
+
+export const updateDiagnozyController = async (req, res) => {
+  let responses = [];
+  await asyncForEach(req.body.items, async (item) => {
+  //req.body.items.forEach(function (item) {
+  const diagnozy =  db.Diagnozy.update(
+    { nazevDiagnoza: item.nazevDiagnoza,
+      popisDiagnoza: item.popisDiagnoza
+     },
+    { where: { id: item.id } }
+  ).then(function(result){
+    responses.push("1");
+    console.log(responses); //returns array OK
+    })
+})
+    console.log('Done');
+    res.status(200).json({message:responses}) //null
+}
+
+//req.body.items.forEach(function (item) {
+/*
+const diagnozy = await db.Diagnozy.update(
+  { nazevDiagnoza: item.nazevDiagnoza,
+    popisDiagnoza: item.popisDiagnoza
+   },
+  { where: { id: req.body.id } }
+)
+  .then(result =>
+    res.status(200).json({message:"Updated successfully"})
+  )
+  .catch(error =>
+    res.status(500).json(error)
+  )
+*/
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
