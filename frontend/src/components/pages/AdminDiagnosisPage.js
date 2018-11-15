e91fd6da09378d2496790f7f850239a0ea19f6fd
Nicole Gross <nicole.gross@student.tuke.sk>
2018-11-15 09:57:37 +0100

    feat admin page diagnosis + connect to db

---
+++ frontend/src/components/pages/AdminDiagnosisPage.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+import {AdminDiagnosisListTemplate} from '../templates/AdminDiagnosisListTemplate';
+import {PageWrapper} from "../organisms/PageWrapper";
+import { startFetchDiagnosis } from '../../services/DiagnosisList/actions';
+
+import {
+  getIsLoading,
+  getDiagnosis,
+  getIsLoaded,
+  getError,
+  getIsError,
+} from '../../services/DiagnosisList/reducer';
+
+class AdminDiagnosisPageRaw extends Component {
+  componentDidMount() {
+    const { startFetchDiagnosis } = this.props;
+    startFetchDiagnosis();
+  }
+
+  render() {
+    const { isLoading, diagnozy, isLoaded, isError, error } = this.props;
+
+    return (
+        <AdminDiagnosisListTemplate
+          isLoading={isLoading}
+          isLoaded={isLoaded}
+          isError={isError}
+          diagnozy={diagnozy}
+          error={error}
+        />
+    );
+  }
+}
+
+const mapStateToProps = storeState => {
+  const { diagnosisList } = storeState;
+  return {
+    isLoading: getIsLoading(diagnosisList),
+    diagnozy: getDiagnosis(diagnosisList),
+    isLoaded: getIsLoaded(diagnosisList),
+    error: getError(diagnosisList),
+    isError: getIsError(diagnosisList),
+  };
+};
+
+const mapDispatchToProps = {
+  startFetchDiagnosis,
+};
+
+export const AdminDiagnosisPage = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AdminDiagnosisPageRaw);
