d8c9c466033c7ab55825a8011a68e4e89e8209ad
Richard Blasko <richard.blasko13@gmail.com>
2018-11-16 00:46:34 +0100

    upraveny formular pre pridanie prevencie

--- frontend/src/components/molecules/AdminPreventionForm.js
+++ frontend/src/components/molecules/AdminPreventionForm.js
@@ -15,7 +15,7 @@ import { Link } from '../atoms/Link';
 
 export class AdminPreventionForm extends Component {
   render() {
-    let {lecimse, anamnesis} = this.props;
+    let {lecimse, anamnesis, diagnosis} = this.props;
     return (
       <Layout className="footer pt-5">
         <Heading level={3} className={"pb-3"}>Preventívni vyšetření</Heading>
@@ -135,6 +135,21 @@ export class AdminPreventionForm extends Component {
                   </Row>
               </Layout>
             </Row>
+            <Heading level={4} className={"pb-3"}>Diagnózy spojené s prevencí</Heading>
+            <Row>
+              <Layout className="col-md-6">
+                <MultiSelectWithLabel
+                  id="diagnosis"
+                  label="Seznam diagnóz"
+                  multi
+                  options={diagnosis.map(diagnosis => {
+                    return {label: diagnosis.nazevDiagnoza, value: diagnosis.nazevDiagnoza}
+                  })}
+                  name="preventions"
+                  placeholder="Vyber..."
+                />
+              </Layout>
+            </Row>
             <Heading level={4} className={"pb-3"}>Výskyt nemocí v rodine</Heading>
             <Row>
               <Layout className="col-md-6">
@@ -148,93 +163,382 @@ export class AdminPreventionForm extends Component {
                   placeholder="Vyber..."
                 />
               </Layout>
-              <Layout className="col-md-6">
-              <Row>
-                <Column md={3}>
-                  <InputWithLabel
-                    id="periodicita"
-                    type="number"
-                    label="Periodicita (v měsících)"
-                    min={1}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekOd"
-                    type="number"
-                    label="Vek od:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekDo"
-                    type="number"
-                    label="Vek do:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-              </Row>
-              <Row>
-                <Column md={3}>
-                  <InputWithLabel
-                    id="periodicita"
-                    type="number"
-                    label="Periodicita (v měsících)"
-                    min={1}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekOd"
-                    type="number"
-                    label="Vek od:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekDo"
-                    type="number"
-                    label="Vek do:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-              </Row>
-              <Row>
-                <Column md={3}>
-                  <InputWithLabel
-                    id="periodicita"
-                    type="number"
-                    label="Periodicita (v měsících)"
-                    min={1}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekOd"
-                    type="number"
-                    label="Vek od:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-                <Column md={2}>
-                  <InputWithLabel
-                    id="vekDo"
-                    type="number"
-                    label="Vek do:"
-                    min={1}
-                    max={120}
-                  />
-                </Column>
-              </Row>
-              </Layout>
             </Row>
+              <Row>
+                <Column md={3}>
+                  <InputWithLabel
+                    id="periodicita"
+                    type="number"
+                    label="Periodicita (v měsících)"
+                    min={1}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekOd"
+                    type="number"
+                    label="Vek od:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekDo"
+                    type="number"
+                    label="Vek do:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={3}>
+                  <InputWithLabel
+                    id="periodicita"
+                    type="number"
+                    label="Periodicita (v měsících)"
+                    min={1}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekOd"
+                    type="number"
+                    label="Vek od:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekDo"
+                    type="number"
+                    label="Vek do:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={3}>
+                  <InputWithLabel
+                    id="periodicita"
+                    type="number"
+                    label="Periodicita (v měsících)"
+                    min={1}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekOd"
+                    type="number"
+                    label="Vek od:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+                <Column md={2}>
+                  <InputWithLabel
+                    id="vekDo"
+                    type="number"
+                    label="Vek do:"
+                    min={1}
+                    max={120}
+                  />
+                </Column>
+              </Row>
+              <Row>
+                <Layout className="col-md-6">
+                  <SelectWithLabel
+                    id="family_anamnes"
+                    label="Seznam anamnéz"
+                    options={anamnesis.map(anamnesis => {
+                      return {label: anamnesis, value: anamnesis}
+                    })}
+                    name="family_anamnes"
+                    placeholder="Vyber..."
+                  />
+                </Layout>
+              </Row>
+                <Row>
+                  <Column md={3}>
+                    <InputWithLabel
+                      id="periodicita"
+                      type="number"
+                      label="Periodicita (v měsících)"
+                      min={1}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekOd"
+                      type="number"
+                      label="Vek od:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekDo"
+                      type="number"
+                      label="Vek do:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                </Row>
+                <Row>
+                  <Column md={3}>
+                    <InputWithLabel
+                      id="periodicita"
+                      type="number"
+                      label="Periodicita (v měsících)"
+                      min={1}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekOd"
+                      type="number"
+                      label="Vek od:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekDo"
+                      type="number"
+                      label="Vek do:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                </Row>
+                <Row>
+                  <Column md={3}>
+                    <InputWithLabel
+                      id="periodicita"
+                      type="number"
+                      label="Periodicita (v měsících)"
+                      min={1}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekOd"
+                      type="number"
+                      label="Vek od:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                  <Column md={2}>
+                    <InputWithLabel
+                      id="vekDo"
+                      type="number"
+                      label="Vek do:"
+                      min={1}
+                      max={120}
+                    />
+                  </Column>
+                </Row>
+                <Row>
+                  <Layout className="col-md-6">
+                    <SelectWithLabel
+                      id="family_anamnes"
+                      label="Seznam anamnéz"
+                      options={anamnesis.map(anamnesis => {
+                        return {label: anamnesis, value: anamnesis}
+                      })}
+                      name="family_anamnes"
+                      placeholder="Vyber..."
+                    />
+                  </Layout>
+                </Row>
+                  <Row>
+                    <Column md={3}>
+                      <InputWithLabel
+                        id="periodicita"
+                        type="number"
+                        label="Periodicita (v měsících)"
+                        min={1}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekOd"
+                        type="number"
+                        label="Vek od:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekDo"
+                        type="number"
+                        label="Vek do:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                  </Row>
+                  <Row>
+                    <Column md={3}>
+                      <InputWithLabel
+                        id="periodicita"
+                        type="number"
+                        label="Periodicita (v měsících)"
+                        min={1}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekOd"
+                        type="number"
+                        label="Vek od:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekDo"
+                        type="number"
+                        label="Vek do:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                  </Row>
+                  <Row>
+                    <Column md={3}>
+                      <InputWithLabel
+                        id="periodicita"
+                        type="number"
+                        label="Periodicita (v měsících)"
+                        min={1}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekOd"
+                        type="number"
+                        label="Vek od:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                    <Column md={2}>
+                      <InputWithLabel
+                        id="vekDo"
+                        type="number"
+                        label="Vek do:"
+                        min={1}
+                        max={120}
+                      />
+                    </Column>
+                  </Row>
+                  <Row>
+                    <Layout className="col-md-6">
+                      <SelectWithLabel
+                        id="family_anamnes"
+                        label="Seznam anamnéz"
+                        options={anamnesis.map(anamnesis => {
+                          return {label: anamnesis, value: anamnesis}
+                        })}
+                        name="family_anamnes"
+                        placeholder="Vyber..."
+                      />
+                    </Layout>
+                  </Row>
+                    <Row>
+                      <Column md={3}>
+                        <InputWithLabel
+                          id="periodicita"
+                          type="number"
+                          label="Periodicita (v měsících)"
+                          min={1}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekOd"
+                          type="number"
+                          label="Vek od:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekDo"
+                          type="number"
+                          label="Vek do:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                    </Row>
+                    <Row>
+                      <Column md={3}>
+                        <InputWithLabel
+                          id="periodicita"
+                          type="number"
+                          label="Periodicita (v měsících)"
+                          min={1}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekOd"
+                          type="number"
+                          label="Vek od:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekDo"
+                          type="number"
+                          label="Vek do:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                    </Row>
+                    <Row>
+                      <Column md={3}>
+                        <InputWithLabel
+                          id="periodicita"
+                          type="number"
+                          label="Periodicita (v měsících)"
+                          min={1}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekOd"
+                          type="number"
+                          label="Vek od:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                      <Column md={2}>
+                        <InputWithLabel
+                          id="vekDo"
+                          type="number"
+                          label="Vek do:"
+                          min={1}
+                          max={120}
+                        />
+                      </Column>
+                    </Row>
             <Heading level={4} className={"pb-3"}>Osobní diagnóza</Heading>
             <Row>
               <Layout className="col-md-6">
--- frontend/src/components/templates/AdminPreventionFormTemplate.js
+++ frontend/src/components/templates/AdminPreventionFormTemplate.js
@@ -4,11 +4,12 @@ import {Layout} from "../atoms/Layout";
 import {AdminPreventionForm} from "../molecules/AdminPreventionForm";
 import {ADMINLECIMSE} from "../../mocks/AdminLecimSe";
 import {ADMINANAMNESIS} from "../../mocks/AdminAnamnesis";
+import {DIAGNOSIS} from "../../mocks/Diagnosis.js";
 
 export const AdminPreventionFormTemplate = ({ title, paragraph }) => (
   <Layout>
     <Layout>
-      <AdminPreventionForm lecimse={ADMINLECIMSE} anamnesis={ADMINANAMNESIS}/>
+      <AdminPreventionForm lecimse={ADMINLECIMSE} anamnesis={ADMINANAMNESIS} diagnosis={DIAGNOSIS}/>
     </Layout>
   </Layout>
 );
