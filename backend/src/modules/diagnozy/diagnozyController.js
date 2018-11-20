a952547b1a2eb877844e2b04d894fcd2923aca44
Anastasi <a.korzhenevskaja@gmail.com>
2018-11-20 16:53:39 +0100

    feat: bulk edit diagnozy, prevence, vysledky

--- backend/src/modules/diagnozy/diagnozyController.js
+++ backend/src/modules/diagnozy/diagnozyController.js
@@ -47,43 +47,21 @@ export const deleteDiagnozyController = async (req, res) => {
         });
 };
 
-
 export const updateDiagnozyController = async (req, res) => {
-  let responses = [];
-  await asyncForEach(req.body.items, async (item) => {
-  //req.body.items.forEach(function (item) {
-  const diagnozy =  db.Diagnozy.update(
-    { nazevDiagnoza: item.nazevDiagnoza,
-      popisDiagnoza: item.popisDiagnoza
-     },
-    { where: { id: item.id } }
-  ).then(function(result){
-    responses.push("1");
-    console.log(responses); //returns array OK
-    })
-})
-    console.log('Done');
-    res.status(200).json({message:responses}) //null
-}
+  const responses = await db.sequelize.transaction(transaction =>
+    Promise.all(
+      req.body.items.map(item =>
+        db.Diagnozy.update(
+          {
+            nazevDiagnoza: item.nazevDiagnoza,
+            popisDiagnoza: item.popisDiagnoza
+          },
+          { where: { id: item.id }, transaction }
+        )
+      )
+    )
+  );
 
-//req.body.items.forEach(function (item) {
-/*
-const diagnozy = await db.Diagnozy.update(
-  { nazevDiagnoza: item.nazevDiagnoza,
-    popisDiagnoza: item.popisDiagnoza
-   },
-  { where: { id: req.body.id } }
-)
-  .then(result =>
-    res.status(200).json({message:"Updated successfully"})
-  )
-  .catch(error =>
-    res.status(500).json(error)
-  )
-*/
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
+  console.log("Done");
+  res.status(200).json({ message: responses });
+};
--- backend/src/modules/seznamVysledku/seznamVController.js
+++ backend/src/modules/seznamVysledku/seznamVController.js
@@ -34,3 +34,27 @@ export const deleteSeznamVController = async (req, res) => {
             res.status(500).json(error);
         });
 };
+
+export const updateSeznamVController = async (req, res) => {
+  const responses = await db.sequelize.transaction(transaction =>
+    Promise.all(
+      req.body.items.map(item =>
+        db.Seznamvysledku.update(
+          {
+            idPrevence: item.idPrevence,
+            idDiagnoza: item.idDiagnoza,
+            vekOd: item.vekOd,
+            vekDo: item.vekDo,
+            pohlavi: item.pohlavi,
+            periodicita: item.periodicita,
+            lecimSeS: item.lecimSeS
+          },
+          { where: { id: item.id }, transaction }
+        )
+      )
+    )
+  );
+
+  console.log("Done");
+  res.status(200).json({ message: responses });
+};
