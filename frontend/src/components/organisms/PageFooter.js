e2e6cd88a394c22edc8004abe7285945b8717e37
Anastasi <a.korzhenevskaja@gmail.com>
2019-01-06 21:02:17 +0100

    footer css

--- frontend/src/components/organisms/PageFooter.js
+++ frontend/src/components/organisms/PageFooter.js
@@ -31,13 +31,7 @@ export class PageFooter extends Component {
             <Heading level={5} className={"pt-2"}>PROČ #PREVENCEMASMYSL?</Heading>
             <Heading level={6} className={"pb-3"}>PROTOŽE PŘECI PREVENCE DÁVÁ SMYSL!</Heading>
             <Paragraph className={"pb-4 text-justify"}>
-              Slovo PREVENCE, které znamená něčemu přecházet… Měla by zahrnovat především naši osobní snahu žít zdravě
-              tak, abychom nemuseli následně podstupovat složitou, mnohdy zdlouhavou cestu zpět ke zdraví, které jsme
-              poztráceli někde na cestě životem. Tak by měl vypadat ideál slova prevence – skutečně předcházet tomu,
-              abychom vůbec onemocněli. V dnešním světě je tomu však trochu naopak. Prevencí obecně se v našem hektickém
-              stylu života zpravidla myslí odhalení toho, zda již náhodou nějakou nemocí v zárodku netrpíme… Ale i to se
-              počítá a často nám tato snaha úřadů, institucí i samotných lékařů může zachránit (a zachraňuje) život. Je
-              však jen na našich bedrech, zda budeme vůči sobě a svému zdraví zodpovědní…
+              Budeme vůči sobě a svému zdraví zodpovědní!
             </Paragraph>
             <Layout className={"float-right"}>
               <ListInline className={"social-network-footer"}>
